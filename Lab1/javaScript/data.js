fad8427a9f2c962d318ca656ac4f52790dbd096c
Chandan <cmahato2000@gmail.com>
2023-03-10 22:33:25 +0545

    sun moon added to flag

--- Lab1/javaScript/data.js
+++ Lab1/javaScript/data.js
@@ -21,11 +21,62 @@ let v11 = [5.75, -0.25, 0];
 let v12 = [-0.25, 4.25, 0];
 let t4 = [...v10, ...v11, ...v12];
 
+//sun
+var sunVertexData = [];
+function createSunVertexList(radius, xo, yo) {
+  let x, y;
+  for (i = 0; i < 360; i += 45) {
+    sunVertexData.push(xo);
+    sunVertexData.push(yo);
+    sunVertexData.push(0);
+    x = radius * Math.cos((Math.PI / 180) * i) + xo;
+    sunVertexData.push(x);
+    y = radius * Math.sin((Math.PI / 180) * i) + yo;
+    sunVertexData.push(y);
+    sunVertexData.push(0);
+    x = radius * Math.cos((Math.PI / 180) * (i + 45)) + xo;
+    sunVertexData.push(x);
+    y = radius * Math.sin((Math.PI / 180) * (i + 45)) + yo;
+    sunVertexData.push(y);
+    sunVertexData.push(0);
+  }
+}
+createSunVertexList(0.75, 1.25, 1.25);
+
+//moon
+var moonVertexData = [];
+function createMoonVertexList(radius, xo, yo) {
+  let x, y;
+  for (i = 0; i > -180; i -= 45) {
+    moonVertexData.push(xo);
+    moonVertexData.push(yo);
+    moonVertexData.push(0);
+    x = radius * Math.cos((Math.PI / 180) * i) + xo;
+    moonVertexData.push(x);
+    y = radius * Math.sin((Math.PI / 180) * i) + yo;
+    moonVertexData.push(y);
+    moonVertexData.push(0);
+    x = radius * Math.cos((Math.PI / 180) * (i - 45)) + xo;
+    moonVertexData.push(x);
+    y = radius * Math.sin((Math.PI / 180) * (i - 45)) + yo;
+    moonVertexData.push(y);
+    moonVertexData.push(0);
+  }
+}
+createMoonVertexList(0.75, 1.25, 4.25);
+
+//processing data
 const vertexData = [];
-const dataArray = [...t1, ...t2, ...t3, ...t4];
+const dataArray = [
+  ...t1,
+  ...t2,
+  ...t3,
+  ...t4,
+  ...sunVertexData,
+  ...moonVertexData,
+];
 for (let i = 0; i < dataArray.length; i++) {
   let y = dataArray[i] / 6.5;
   vertexData.push(y);
 }
-
 console.log(vertexData);
